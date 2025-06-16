1be9e6f74395341e717c6251fa9dcbdf7089277a
sthaSandesh <sandeshstha561@gmail.com>
2025-06-16 14:36:29 +0545

    home bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           playsInline
           className="absolute inset-0 w-full h-full object-cover z-0"
         >
-          <source src="/homebg.mp4" type="video/mp4" />
+          <source src="/Homebg.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
